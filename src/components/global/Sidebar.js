6feb838fbfb83b4016868ea64115120054521a2c
TALLER\Leo <leonardo@smartnet.com.ar>
2018-05-15 16:24:13 -0300

    agregando componente sidebar

---
+++ src/components/global/Sidebar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './css/Sidebar.css';
+
+class Sidebar extends Component {
+  render() {
+    return (
+      <div className="Sidebar">
+        <h3><p>Leonardo Casales</p></h3>
+        <p>DNI 30.195.728</p>
+      </div> 
+    );
+  }
+}
+
+export default Sidebar;
