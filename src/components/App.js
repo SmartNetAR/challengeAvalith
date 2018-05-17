47d70987fab264afd477f4f3c61dca5a24c02d08
TALLER\Leo <leonardo@smartnet.com.ar>
2018-05-16 22:54:02 -0300

    sidebar added

--- src/components/App.js
+++ src/components/App.js
@@ -3,24 +3,17 @@ import React, { Component } from 'react';
 //componentes
 import Header from './global/Header';
 import Content from './global/Content';
-import { Grid, Row, Col } from 'react-bootstrap';
+import { Grid, Row, Col, Navbar } from 'react-bootstrap';
 import Sidebar from './global/Sidebar';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      
+      <div> className="App">
         <Header />
-        <Grid>
-          <Row>
-            <Col xs={100} md={3}>
-                <Sidebar />
-            </Col>
-            <Col xs={100} me={9}>
-                <Content />
-            </Col>
-          </Row>
-        </Grid>
+        <Sidebar />
+        <Content />        
       </div> 
     );
   }
--- src/components/global/Sidebar.js
+++ src/components/global/Sidebar.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
 import './css/Sidebar.css';
+import { Checkbox, ButtonGroup, Button } from 'react-bootstrap';
 
 class Sidebar extends Component {
+  
   render() {
     return (
-      <div className="Sidebar">
-        <h3><p>Leonardo Casales</p></h3>
-        <p>DNI 30.195.728</p>
+      <div className="sidenav">
+        <div className="devname">
+          <div className="font-dev-name"><p>DEV NAME</p></div>
+          <div className="font-dev-details">
+            <p>DNI 30.195.728</p>
+            <p>35 YEARS OLD</p>
+          </div>
+        </div>
+        <input></input>
+        <button>Find</button>
+        <form>
+          <div className="radio">
+            <label>
+              <input type="radio" value="option1" checked={true} />
+              Front
+            </label>
+          </div>
+          <div className="radio">
+            <label>
+              <input type="radio" value="option2" />
+              Back
+            </label>
+          </div>
+        </form>
+
+
       </div> 
     );
   }
