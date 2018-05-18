d575dace9eadc0900952fbe4b176c83514ed947b
SmartNetAR <leonardo@smartnet.com.ar>
2018-05-17 22:58:51 -0300

    card loaded with json data

--- src/components/global/Content.js
+++ src/components/global/Content.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
 import './css/Content.css';
 import Card from './Card';
-import jsonRecived from './data/cards.json'
+import jsonRecived from '../../data/cards.json'
+
 
 class Content extends Component {
+
   render() {
     return (
       <div className="Content">
-        
         <header className="Title">LANGUAGES & TECHNOLOGIES</header>
+        {/* {console.log(jsonRecived)} */}
 
-        <Card />
-        <Card />
-        <Card />
+          {jsonRecived && jsonRecived.map( (item, key) => 
+          <Card 
+            key= {key}
+            title= {item.cardTitle} 
+            details={item.cardDescription}
+            technology={item.cardTechnology}
+            imageUrl={item.cardImageUrl}
+          />)}
+
+
+        {/* <Card title="React" details="biblioteca de Facebook"/>
+        <Card title="Angular" details="framwork de Google"/>
+        <Card title="View" details="biblioteca en crecimiento"/> */}
+        {/* <ul>
+          {jsonRecived && jsonRecived.map( (item, key) => <li key = {key}>{item.cardTitle}</li> )}
+        </ul> */}
       </div> 
     );
   }
