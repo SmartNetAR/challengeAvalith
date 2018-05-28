82ee67d78cdb247ce0ddf98315932da8fadbd31c
Leonardo Casales <leonardo@smartnet.com.ar>
2018-05-28 13:10:41 -0300

    styles radio and textfield applied

--- src/components/global/Controls.js
+++ src/components/global/Controls.js
@@ -6,66 +6,86 @@ import Radio from '@material-ui/core/Radio';
 import RadioGroup from '@material-ui/core/RadioGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormControl from '@material-ui/core/FormControl';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+// import WithTheme from './WithTheme';
+
+const styles = theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  cssLabel: {
+    '&$cssFocused': {
+      color: '#ffffff',
+    },
+  },
+  cssFocused: {},
+  cssUnderline: {
+    '&:after': {
+      borderBottomColor: '#ffffff',
+    },
+  },
+
+});
+
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark', // Switching the dark mode on is a single property value change.
+
+  },
+})
+
+
 
 class Controls extends Component {
   constructor() {
     super(); 
+
     this.handleChangeInput = this.handleChangeInput.bind(this);
   }
-
+  state = {
+    value: '',
+    color: 'primary'
+  };
   handleChangeInput(e) {
     this.props.controlFilter(e);
   }
 
+  handleChange = event => {
+    this.setState({ value: event.target.value });
+    this.props.controlFilter(event);
+  };
+
   render() {
     return (
-      <div className="controls"> {/*</div> style={{backgroundColor:"white"}}>*/}
-         {/* <div className="editSearch">
-          <input id="txtSearch" placeholder='Search in cards'
-          onChange={this.handleChangeInput}></input>
-        </div> */}
-        {/* <div className="editSearch"> */}
-          <TextField 
+      <div className="controls" > {/*</div> style={{backgroundColor:"white"}}>*/}
+        <MuiThemeProvider theme={theme}>
+          <TextField
             className="editSearch"
-            label="Search in Cards" 
+            // label="Search in Cards"
             placeholder="Search in Cards"
             onChange={this.handleChangeInput}/>
-        {/* </div> */}
         <div className="radio">
           <p>Filter by:</p>
-          <form>
-            <label>
-              <input id="front" type="radio" value='Frontend' name="technologie"
-              onClick={this.handleChangeInput} />
-              Frontend
-            </label>
-            <label>
-              <br/>
-              <input id="back" type="radio" value='Backend' name="technologie"
-              onClick={this.handleChangeInput} />
-              Backend
-            </label>
-          </form>
-        </div>
-
-
-
-<RadioGroup
+          {/* <FormLabel component="legend">Filter by</FormLabel> */}
+          
+          <RadioGroup
             aria-label="gender"
             name="gender1"
             // // className={classes.group}
-            // value={this.state.value}
-            // onChange={this.handleChange}
+            value={this.state.value}
+            onChange={this.handleChange}
           >
-            <FormControlLabel value='Frontend' name="technologie" control={<Radio />} label="Frontend" />
-            <FormControlLabel value='Backend' name="technologie" control={<Radio />} label="Backend" />
+            <FormControlLabel id="front" value='Frontend' name="technologie" control={<Radio color="white" />} label="Frontend" />
+            <FormControlLabel id="back" value='Backend' name="technologie" control={<Radio  color="white" />} label="Backend" />
 
           </RadioGroup>
+          
 
 
-
+      </div>
+      </MuiThemeProvider>
         </div>
-      //</div> 
     );
   }
 }
